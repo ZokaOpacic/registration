a0b8914d1bef96f6a38fb487740d386f82deb981
ZokaOpacic <opacic83zo@gmail.com>
2022-03-05 13:32:34 +0100

    izmena registracije

--- cypress/integration/register.spec.js
+++ cypress/integration/register.spec.js
@@ -1,11 +1,8 @@
 describe('register case', () => {
-    it('visit gallery app', () => {
-        cy.visit('https://gallery-app.vivifyideas.com/')
-    })
-    it('click register button', () => {
-        cy.get('a[href="/register"]').click()
-    })
+
     it('register with spaces', () => {
+        cy.visit('/');
+        cy.get('a[href="/register"]').click();
         cy.get('input[id = "first-name"]').type('     ');
         cy.get('input[id = "last-name"]').type('     ');
         cy.get('input[id = "email"]').type('83opacic83@gmail.com');
@@ -14,13 +11,10 @@ describe('register case', () => {
         cy.get(':checkbox').check();
         cy.get('button').click();
     })
-    it('visit gallery app', () => {
-        cy.visit('https://gallery-app.vivifyideas.com/')
-    })
-    it('click register button', () => {
-        cy.get('a[href="/register"]').click()
-    })
+
     it('email without ".com"', () => {
+        cy.visit('/');
+        cy.get('a[href="/register"]').click();
         cy.get('input[id = "first-name"]').type('Zorica');
         cy.get('input[id = "last-name"]').type('Opacic');
         cy.get('input[id = "email"]').type('83opacic83@gmail');
@@ -29,13 +23,10 @@ describe('register case', () => {
         cy.get(':checkbox').check();
         cy.get('button').click();
     })
-    it('visit gallery app', () => {
-        cy.visit('https://gallery-app.vivifyideas.com/')
-    })
-    it('click register button', () => {
-        cy.get('a[href="/register"]').click()
-    })
+
     it('email without "@"', () => {
+        cy.visit('/');
+        cy.get('a[href="/register"]').click();
         cy.get('input[id = "first-name"]').type('Zorica');
         cy.get('input[id = "last-name"]').type('Opacic');
         cy.get('input[id = "email"]').type('83opacic83gmail.com');
@@ -44,13 +35,10 @@ describe('register case', () => {
         cy.get(':checkbox').check();
         cy.get('button').click();
     })
-    it('visit gallery app', () => {
-        cy.visit('https://gallery-app.vivifyideas.com/')
-    })
-    it('click register button', () => {
-        cy.get('a[href="/register"]').click()
-    })
+
     it('Already registrated email', () => {
+        cy.visit('/');
+        cy.get('a[href="/register"]').click();
         cy.get('input[id = "first-name"]').type('Zorica');
         cy.get('input[id = "last-name"]').type('Opacic');
         cy.get('input[id = "email"]').type('marabuba@gmail.com');
@@ -59,13 +47,10 @@ describe('register case', () => {
         cy.get(':checkbox').check();
         cy.get('button').click();
     })
-    it('visit gallery app', () => {
-        cy.visit('https://gallery-app.vivifyideas.com/')
-    })
-    it('click register button', () => {
-        cy.get('a[href="/register"]').click()
-    })
+
     it('Password with 8 letters', () => {
+        cy.visit('/');
+        cy.get('a[href="/register"]').click();
         cy.get('input[id = "first-name"]').type('Zorica');
         cy.get('input[id = "last-name"]').type('Opacic');
         cy.get('input[id = "email"]').type('83opacic83@gmail.com');
@@ -74,13 +59,10 @@ describe('register case', () => {
         cy.get(':checkbox').check();
         cy.get('button').click();
     })
-    it('visit gallery app', () => {
-        cy.visit('https://gallery-app.vivifyideas.com/')
-    })
-    it('click register button', () => {
-        cy.get('a[href="/register"]').click()
-    })
+
     it('password with 7 characters', () => {
+        cy.visit('/');
+        cy.get('a[href="/register"]').click();
         cy.get('input[id = "first-name"]').type('Zorica');
         cy.get('input[id = "last-name"]').type('Opacic');
         cy.get('input[id = "email"]').type('83opacic83@gmail.com');
@@ -89,13 +71,10 @@ describe('register case', () => {
         cy.get(':checkbox').check();
         cy.get('button').click();
     })
-    it('visit gallery app', () => {
-        cy.visit('https://gallery-app.vivifyideas.com/')
-    })
-    it('click register button', () => {
-        cy.get('a[href="/register"]').click()
-    })
+
     it('passwors and confirmed password does not match', () => {
+        cy.visit('/');
+        cy.get('a[href="/register"]').click();
         cy.get('input[id = "first-name"]').type('Zorica');
         cy.get('input[id = "last-name"]').type('Opacic');
         cy.get('input[id = "email"]').type('83opacic83@gmail.com');
@@ -104,13 +83,10 @@ describe('register case', () => {
         cy.get(':checkbox').check();
         cy.get('button').click();
     })
-    it('visit gallery app', () => {
-        cy.visit('https://gallery-app.vivifyideas.com/')
-    })
-    it('click register button', () => {
-        cy.get('a[href="/register"]').click()
-    })
+
     it('registration with valid credentials', () => {
+        cy.visit('/');
+        cy.get('a[href="/register"]').click();
         cy.get('input[id = "first-name"]').type('Zorica');
         cy.get('input[id = "last-name"]').type('Opacic');
         cy.get('input[id = "email"]').type('83opacic83@gmail.com');
